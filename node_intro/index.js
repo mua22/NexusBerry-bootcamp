dcb25814fab52eb7b77c88d42535bf6d072c3585
Usman Akram <usman.akram@gmail.com>
2019-11-02 01:51:19 -0700

    our first module

---
+++ node_intro/index.js
@@ -0,0 +1,4 @@
+console.log("Hello Nexus Berry");
+const nexus = require("./nexusberry");
+
+nexus();
---
+++ node_intro/nexusberry.js
@@ -0,0 +1,5 @@
+function helloNexusBerry() {
+  console.log("Hello from nexus berry funcion");
+}
+
+module.exports = helloNexusBerry;
