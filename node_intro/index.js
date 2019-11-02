8bbb12538cc872f714fea0711ad8c66ba12699df
Usman Akram <usman.akram@gmail.com>
2019-11-02 02:50:29 -0700

    http server created

--- node_intro/index.js
+++ node_intro/index.js
@@ -1,4 +1,8 @@
 console.log("Hello Nexus Berry");
 const nexus = require("./nexusberry");
-
+const nexusUsman = require("nexus-usman");
+const { usman, akram } = require("./multipleExports");
 nexus();
+usman();
+akram();
+nexusUsman();
---
+++ node_intro/multipleExports.js
@@ -0,0 +1,11 @@
+function usman() {
+  console.log("Usman");
+}
+function akram() {
+  console.log("Akram");
+}
+
+module.exports = {
+  usman: usman,
+  akram: akram
+};
