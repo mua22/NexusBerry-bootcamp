68ef725d948c8f24d7eeab4794d9051554349c81
Usman Akram <usman.akram@gmail.com>
2019-11-09 01:25:02 -0800

    restful api

--- Lecture 3 Express Generator/ecommerce-express/app.js
+++ Lecture 3 Express Generator/ecommerce-express/app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var productsRouter = require("./routes/products");
+
 var app = express();
 
 // view engine setup
--- Lecture 3 Express Generator/ecommerce-express/routes/products.js
+++ Lecture 3 Express Generator/ecommerce-express/routes/products.js
@@ -6,5 +6,28 @@ var products = ["Pen", "Pencil", "Coke"];
 router.get("/", function(req, res, next) {
   res.send(products);
 });
+// get Single Products
+router.get("/:id", function(req, res, next) {
+  if (!products[req.params.id]) {
+    return res.status(404).send("Product Not Found");
+  }
+  res.send(products[req.params.id]);
+});
 
+//delete a product
+router.delete("/:id", (req, res) => {
+  products.splice(req.params.id, 1);
+  res.send(products);
+});
+//create a product
+router.post("/", (req, res) => {
+  console.log(req.body);
+  products.push(req.body.name);
+  res.send(products);
+});
+//patching
+router.patch("/:id", (req, res) => {
+  products[req.params.id] = req.body.name;
+  res.send(products);
+});
 module.exports = router;
